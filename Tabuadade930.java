e2fe3f2f492b04b77c8d72efebbb1d624ae81667
GGG <127780046+GGG710@users.noreply.github.com>
2023-03-16 19:31:35 +0000

    Pending changes exported from your codespace

---
+++ Tabuadade930.java
@@ -0,0 +1,11 @@
+public class Tabuadade930 {
+ public static void main(String[] args) {
+    
+    System.out.println("Tabuada de 930");
+
+    int numTabuada = 930;
+    for(int y=0;y <=10;y++)
+        System.out.println(y*numTabuada);
+ }
+    
+}
